ac6be314024a1a7840320ed59c4ec010da54b65f
George Daniel <george.daniel>
2018-06-10 12:47:28 +0800

    sample back end function added to test token based authorization

---
+++ OwinAuthenticationWithAngular.API/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using OwinAuthenticationWithAngular.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace OwinAuthenticationWithAngular.API.Controllers
+{
+    [RoutePrefix("api/Orders")]
+    public class OrderController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order {Id = 10248, CustomerName = "Lazada", CustomerAddress = "Hougang", IsShipped = true },
+                new Order {Id = 10249, CustomerName = "Qoo10", CustomerAddress = "Yishun", IsShipped = false},
+                new Order {Id = 10250,CustomerName = "Amazon", CustomerAddress = "Woodlands", IsShipped = false },
+                new Order {Id = 10251,CustomerName = "Carousel", CustomerAddress = "Ang Mo Kio", IsShipped = false},
+                new Order {Id = 10252,CustomerName = "Zalora", CustomerAddress = "Raffles Place", IsShipped = true}
+            };
+
+            return Ok(orders);
+        }
+    }
+}
---
+++ OwinAuthenticationWithAngular.API/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwinAuthenticationWithAngular.API.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerAddress { get; set; }
+        public bool IsShipped { get; set; }
+    }
+}
